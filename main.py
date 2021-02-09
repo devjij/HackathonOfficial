47ebb965c1f8701c072a72f4a251e5bf1b4b0795
Dev Jijilal <djijilal@gmail.com>
2021-02-09 08:20:55 -0500

    Made it so you can find ascii from sentence

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
 # This is a sample Python script.
+
+sentence = input("Please enter a sentence to decode:\n")
+
+str_length = len(sentence)
+
+x = 0
+while x <= str_length - 1:
+    print(ord(sentence[x]))
+    x = x + 1
+
 print("This projects for Hackathon")
 print("This is a project made by Dev, Sam and Jacob.")
